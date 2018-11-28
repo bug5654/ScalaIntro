96d7f496c72f12b018334644f0ef735c3027b427
bug5654 <bug.new@gmail.com>
2018-11-27 17:13:42 -0800

    updated to infix notation

--- ComplexNumbers.scala
+++ ComplexNumbers.scala
@@ -1,11 +1,15 @@
 class Complex(real: Double, complex: Double) {
-	def re() = real
-	def j() = complex
+	// def re() = real
+	// def j() = complex
+	def re = real
+	def j = complex
 }
 
 object ComplexNumbers {
 	def main(args: Array[String]) {
 		val c = new Complex(1.2, 3.4)
-		println("imaginary part: " + c.j()) 
+		// println("imaginary part: " + c.j()) 
+		println("imaginary part: " + (c j) )
+
 	}
 }
