c5cab1ce0fe3370477b1e4e9d27dd30f16990f79
bug5654 <bug.new@gmail.com>
2018-11-28 16:37:41 -0800

    overriding methods

--- ComplexNumbers.scala
+++ ComplexNumbers.scala
@@ -3,13 +3,21 @@ class Complex(real: Double, complex: Double) {
 	// def j() = complex
 	def re = real
 	def j = complex
+	override def toString() = "" + (if (re != 0) {re+" "} else "") + (if (j < 0 || re ==0) "" else " + ") + j + "j" 
+	//would create sign() function to align negatives properly but simple example
 }
 
 object ComplexNumbers {
 	def main(args: Array[String]) {
 		val c = new Complex(1.2, 3.4)
+		val d = new Complex(0,2)
+		val f = new Complex(3,-4)
+
 		// println("imaginary part: " + c.j()) 
-		println("imaginary part: " + (c j) )
+		// println("imaginary part: " + (c j) )
+		println("complex number:  " + c)
+		println("complex number:  " + d)
+		println("complex number:  " + f)
 
 	}
 }
